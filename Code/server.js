4c20d8b63c7adc8c5d33662a007d8f999a52374d
Carinn Gaspar <gasca681@student.otago.ac.nz>
2024-04-08 21:46:53 +1200

    Merging to main branch opps

--- Code/server.js
+++ Code/server.js
@@ -6,6 +6,7 @@ const server = createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
