63f838a14978239abdc665a868b376a0fde8d742
lumdy690 <lumdy690@student.otago.ac.nz>
2024-04-08 21:45:33 +1200

    sql

---
+++ sql/setup.sql
@@ -0,0 +1,44 @@
+
+
+DROP TABLE IF EXISTS Group;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Task;
+DROP TABLE IF EXISTS GroupUser;
+DROP TABLE IF EXISTS GroupTask;
+
+
+CREATE TABLE user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE group (
+    group_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+);
+CREATE TABLE task (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE group_user (
+    user_id INT,
+    group_id INT,
+    PRIMARY KEY (user_id, group_id),
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (group_id) REFERENCES group(group_id)
+);
+
+CREATE TABLE group_task (
+    status VARCHAR(255) NOT NULL,
+    task_id INT,
+    user_id INT,
+    FOREIGN KEY (task_id) REFERENCES task(id),
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+
+
