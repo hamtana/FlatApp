b3e8d843a0348dc85f2882537e2aaca9b70482ab
lumdy690 <lumdy690@student.otago.ac.nz>
2024-04-08 22:26:36 +1200

    Adding local host in printline to CLI for ease of user

--- Code/app.js
+++ Code/app.js
@@ -33,5 +33,5 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port http://${port}`);
+    console.log(`Server is running on port http://localhost:${port}`);
 });
