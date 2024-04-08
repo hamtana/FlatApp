daf8429d8c545408fe903873386490894bc063f0
Carinn Gaspar <gasca681@student.otago.ac.nz>
2024-04-08 22:26:30 +1200

    Neatened up the app.js file. Please keep tidy

--- Code/app.js
+++ Code/app.js
@@ -1,37 +1,56 @@
 const express = require('express');
 
 const path = require('path');
-
 const app = express();
 const port = 3000;
 
+// ==============================================
+/** All the app.uses go here.
+ *  - These link to different routers which control different aspects of the app
+ */
+// ==============================================
 app.use(express.static('public'));
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
 app.use('/path', require('./path_router'));
+// ==============================================
 
-// Define a route
-app.get('/', (req, res) => {
+
+
+// ==============================================
+/** This section starts the app an has any error handling routes on the
+ *  base level.
+ */
+// ==============================================
+
+// Define the route if someone goes somewhere where there is no html file
+app.get('*', (req, res) => {
     // This directs the router to the specific file
     res.sendFile(path.join(__dirname, 'public','index.html'));
 });
 
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
+
+// ==============================================
 
 
 
 
+
+
+
+// ==============================================
+/** Unused Code goes here. 
+ *  
+ * 
+ */
+// ==============================================
+//app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
 
 // Define a route
 // app.get('/', (req, res) => {
 //     res.send('Hello World!');
 // });
-
-
-
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on port http://${port}`);
-});
+// ==============================================
