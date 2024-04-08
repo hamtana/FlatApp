f9ccca0256026bfda6299c435a2911bbd5d543c4
Carinn Gaspar <gasca681@student.otago.ac.nz>
2024-04-08 22:06:30 +1200

    Added index,html get.

--- Code/app.js
+++ Code/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 // Define a route
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    // This directs the router to the specific file
+    res.sendFile(path.join(__dirname, 'public','index.html'));
 });
 
 // Start the server
