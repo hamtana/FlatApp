379408fbc6b6e6e2336883f2fc623f1e556a36ab
Hamish Phillips <phiha428@student.otago.ac.nz>
2024-04-18 11:04:34 +1200

    fix app.js port

--- Code/app.js
+++ Code/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // ==============================================
 /** All the app.uses go here.
