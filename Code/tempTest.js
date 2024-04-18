f1fcb73016e2c1ac1b83e6f0f521678735cb1c51
Carinn Gaspar <gasca681@student.otago.ac.nz>
2024-04-18 19:46:29 +1200

    Hopefully properly pushed now

---
+++ Code/tempTest.js
@@ -0,0 +1,7 @@
+// Define the sum function
+function sum(a, b) {
+    return a + b;
+}
+
+// Export the sum function to make it accessible in other modules
+module.exports = sum;
--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,4 @@
-const sum = require('../app');
+const sum = require('../Code/tempTest.js');
 
 test('adds 1 + 3 to equal 3', () => {
 	expect(sum(1,2)).toBe(3);
