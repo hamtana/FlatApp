52c6828dcbad4e56ac25a65277db912721aa100a
lumdy690 <lumdy690@student.otago.ac.nz>
2024-04-27 19:30:17 +1200

    Adding Functions To Check User Is logged in

--- path_router.js
+++ path_router.js
@@ -51,6 +51,10 @@ router.get('/createIndividualTask', async (req, res) => {
     res.render('createIndividualTask');
 });
 
+router.get('/viewYourTask', async (req, res) => {
+    res.render('viewYourTask');
+});
+
 router.get('/login', async (req, res) => {
     res.render('login');
 });
@@ -189,5 +193,29 @@ router.get('/returnTable', async (req, res) => {
 
     });
   });
+
+
+
+var getUserOrLogin = function (req, res, next) {
+    var user = req.session.user;
+  
+    if (user == null) {
+      req.session.backTo = req.originalUrl; 
+      res.redirect('/login');
+    } else {
+      req.user = user;
+      next();
+    }
+  };
+
+  var getUser = function (req) {
+    var user = req.session.user;
+  
+    if (user == null) {
+      throw('Error');
+    } else {
+      return user;
+    }
+  };
 
 module.exports = router;
