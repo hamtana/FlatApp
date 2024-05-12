53d9ae3bf30616c305471ed0062ebc7abe8faf0b
lumdy690 <lumdy690@student.otago.ac.nz>
2024-05-12 13:57:14 +1200

    Deleted node modules and reinstalled #26

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const path = require('path');
 const session = require('express-session')
 
 
+
 const app = express();
+app.set('trust proxy', 1) // trust first proxy
 app.use(session({
 	secret: 'secret',
 	resave: true,
